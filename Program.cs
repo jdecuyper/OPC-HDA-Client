d1ad37a80faccba5387492358c949cdf302ad1ec
jdecuyper <jerome.decuyper@gmail.com>
2015-06-30 17:58:39 -0500

    Connect to an HDA server and then disconnect

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPC_HDA_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverName = "OPCSample.OpcHdaServer";
+            Opc.URL url = new Opc.URL(String.Format("opchda://localhost/{0}", serverName));
+            OpcCom.Factory fact = new OpcCom.Factory();
+            Opc.Hda.Server hdaServer = new Opc.Hda.Server(fact, url);
+            hdaServer.Connect();
+            hdaServer.Disconnect();
+        }
+    }
+}
